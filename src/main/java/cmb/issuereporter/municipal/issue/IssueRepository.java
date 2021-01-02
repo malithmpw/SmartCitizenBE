e18d38686bcad766b91382ee9b1494c2ae1cd607
heshan <h.r.randika@gmail.com>
2021-01-02 21:42:50 +0530

    date filter issue fixed

--- src/main/java/cmb/issuereporter/municipal/issue/IssueRepository.java
+++ src/main/java/cmb/issuereporter/municipal/issue/IssueRepository.java
@@ -15,7 +15,7 @@ public interface IssueRepository extends JpaRepository<Issue, Integer> {
 
     @Query("SELECT i FROM Issue i WHERE (:areaId is null or i.area.id = :areaId) and (:categoryId is null or i.category.id = :categoryId) and " +
             "(:userId is null or i.user.id = :userId) and (:status is null or i.status = :status) and (:userId is null or i.user.id = :userId) and " +
-            "(:assigneeId is null or i.assignee.id = :assigneeId) and (:assignById is null or i.assignBy.id = :assignById) and Date(i.createdDate) <= :startDate AND Date(i.createdDate) >= :endDate")
+            "(:assigneeId is null or i.assignee.id = :assigneeId) and (:assignById is null or i.assignBy.id = :assignById) and Date(i.createdDate) >= :startDate AND Date(i.createdDate) <= :endDate")
     Page<Issue> findIssue(@Param("areaId") Integer areaId, @Param("categoryId") Integer categoryId, @Param("userId") Integer userId,
                           @Param("status") String status, @Param("assigneeId") Integer assigneeId, @Param("assignById") Integer assignById,
                           @Param("startDate") Date startDate, @Param("endDate") Date endDate,  Pageable pageable);
